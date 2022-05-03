db253981aca7b00a2fa9093963928c60b9400465
Sagar <worthless.dumboo@gmail.com>
2022-05-03 15:48:39 -0500

    Reverse Word: String reversed in O(n) but string is reversed instead of word

--- src/lab7/WordReverseInString.java
+++ src/lab7/WordReverseInString.java
@@ -15,6 +15,7 @@ import java.util.Stack;
 
 public class WordReverseInString {
 
+    /* O(n2) */
     public static String reverseEveryWordInString(String string){
 
         String [] words = string.split(" ");
@@ -30,9 +31,33 @@ public class WordReverseInString {
         return reversed.toString();
     }
 
+    public static String On(String string){
+
+        char[] chars = string.toCharArray();
+        Stack stack = new Stack();
+
+        for(int i=0;i<chars.length;i++){
+            stack.push(chars[i]);
+        }
+        char [] reversed = new char[chars.length];
+        for(int i = chars.length-1; i>=0; i--){
+            char ch = (char)stack.pop();
+            reversed[i] = ch;
+        }
+        string = String.valueOf(reversed);
+        String returnString = "";
+        String []words = string.split("");
+
+        for(int i=0 ; i<words.length; i++){
+            returnString += words[words.length-1-i];
+        }
+        return returnString;
+    }
+
     public static void main(String[] args) {
 
         System.out.println(reverseEveryWordInString("This is a test"));
+        System.out.println(On("This is a test"));
     }
 
 }
