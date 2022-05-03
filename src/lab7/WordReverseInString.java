98e0ae4f25bdb18bfa11f3664702298040209e56
Sagar <worthless.dumboo@gmail.com>
2022-05-03 16:27:17 -0500

    Each word reversed in O(n)

--- src/lab7/WordReverseInString.java
+++ src/lab7/WordReverseInString.java
@@ -1,7 +1,9 @@
 package lab7;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 /**
  * Devise an O(n) algorithm to accomplish this task:
@@ -34,24 +36,20 @@ public class WordReverseInString {
     public static String On(String string){
 
         char[] chars = string.toCharArray();
-        Stack stack = new Stack();
 
-        for(int i=0;i<chars.length;i++){
-            stack.push(chars[i]);
+        String st ="";
+        for(int i =chars.length-1; i >=0 ; i--){
+            st += chars[i];
         }
-        char [] reversed = new char[chars.length];
-        for(int i = chars.length-1; i>=0; i--){
-            char ch = (char)stack.pop();
-            reversed[i] = ch;
-        }
-        string = String.valueOf(reversed);
-        String returnString = "";
-        String []words = string.split("");
+        String [] words = st.split(" ");
+        st ="";
 
-        for(int i=0 ; i<words.length; i++){
-            returnString += words[words.length-1-i];
+        int i=words.length-1;
+        for(String word: words){
+            st += words[i]+ " ";
+            i--;
         }
-        return returnString;
+        return st.trim();
     }
 
     public static void main(String[] args) {
