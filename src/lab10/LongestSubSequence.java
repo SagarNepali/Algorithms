8a3113ab050da7bd169fe4a374d5de88a4644d88
Sagar <worthless.dumboo@gmail.com>
2022-05-11 14:39:29 -0500

    Recursive Longest Common Subsequence with dynamic programming

--- src/lab10/LongestSubSequence.java
+++ src/lab10/LongestSubSequence.java
@@ -11,6 +11,8 @@ package lab10;
  */
 public class LongestSubSequence {
 
+    static int D[][] = null;
+
     static int iterativeLCS(String s1, String s2){
 
         int n = s1.length() ;
@@ -21,7 +23,7 @@ public class LongestSubSequence {
 
         for(int i=1; i<= n; i++){
             for(int j=1; j<=m; j++){
-                 if(s1.charAt(n-i) == s2.charAt(m-j)){
+                 if(s1.charAt(n-i) == s2.charAt(m-j)){ //checking character from last
                     D[i][j]= D[i-1][j-1] + 1;
                 }else{
                     D[i][j] = Math.max(D[i][j-1]
@@ -32,8 +34,38 @@ public class LongestSubSequence {
         return D[n][m];
     }
 
+    static int lcs(String s1, String s2){
+
+        if(s1.length()==0 || s2.length()==0) return 0;
+
+        D = new int[s1.length()+1][s2.length()+1];
+        return recursiveLCS(s1,s2);
+    }
+
+    static int recursiveLCS(String s1, String s2){
+
+        int n = s1.length();
+        int m = s2.length();
+
+        if(n==0 || m==0) return 0;
+
+        else if(s1.charAt(n-1) == s2.charAt(m-1)) {
+            D[n][m] = recursiveLCS(s1.substring(0,n-1), s2.substring(0,m-1)) +1;
+        }else{
+            D[n][m] = Math.max(
+                    recursiveLCS(s1.substring(0,n-1),s2),
+                    recursiveLCS(s1, s2.substring(0,m-1))
+            );
+        }
+        return D[n][m];
+    }
+
+
     public static void main(String[] args) {
         System.out.println("LCS of abc and ac is : "+iterativeLCS("abc","ac"));
         System.out.println("LCS of abc and ac is : "+iterativeLCS("regular","ruler"));
+
+        System.out.println("Recursive LCS of abc and ac is : "+lcs("abc","ac"));
+        System.out.println("Recursive LCS of regular and ac ruler : "+lcs("regular","ruler"));
     }
 }
